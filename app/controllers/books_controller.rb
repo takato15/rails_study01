83c70ea9b1f7c867b0e9081f0de5a3c27c8fe90a
“takato15” <gshawks21@gmail.com>
2021-12-21 17:15:17 +0000

    [Update]flash機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,8 +8,7 @@ class BooksController < ApplicationController
     def create
         @book = Book.new(book_params)
         if @book.save
-          flash[:notice] = "Book was successfully created."
-          redirect_to book_path(@book.id)
+          redirect_to book_path(@book.id), notice: "Book was successfully created."
         else
           render :index
         end
@@ -24,7 +23,18 @@ class BooksController < ApplicationController
     end
     
     def update
-        @book = Book.update(book_params)
+        @book = Book.find(params[:id])
+        if @book.update(book_params)
+          redirect_to book_path(@book.id), notice: "Book was successfully updated."
+        else
+          render :edit
+        end
+    end
+    
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path, notice: "Book was successfully deleted."
     end
     
     private
