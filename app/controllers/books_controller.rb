9b03c4d3063f7f6929fc5d7fbab814636372d48e
“takato15” <gshawks21@gmail.com>
2021-12-21 16:10:10 +0000

    [Add]新規投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,31 @@ class BooksController < ApplicationController
     end
     
     def create
+        @book = Book.new(book_params)
+        if @book.save
+          flash[:notice] = "Book was successfully created."
+          redirect_to book_path(@book.id)
+        else
+          render :index
+        end
+    end
+    
+    def show
+        @book = Book.find(params[:id])
+    end
+    
+    def edit
+        @book = Book.find(params[:id])
+    end
+    
+    def update
+        @book = Book.update(book_params)
+    end
+    
+    private
+    
+    def book_params
+        params.require(:book).permit(:title, :body)
     end
     
 end
